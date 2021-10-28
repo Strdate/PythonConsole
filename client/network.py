3d455e33a3a640221c39dde7bff152023ba9d550
HuangFuSL <huangfusl@outlook.com>
2021-10-28 10:33:26 +0800

    Added logging support

--- client/network.py
+++ client/network.py
@@ -5,6 +5,7 @@ Socket-based networking for communicating with Cities: Skylines
 from __future__ import annotations
 
 import asyncio
+import logging
 import socket
 import struct
 
@@ -15,6 +16,7 @@ class AsyncSocketHandler():
     def __init__(self, host: str, port: int):
         self._sock = socket.socket()
         self._sock.bind((host, port))
+        logging.info(f"Listening on {host} port {port}")
         self._sock.listen()
         self._conn = None
 
@@ -22,29 +24,36 @@ class AsyncSocketHandler():
         """ Accept an incoming connection """
         loop = asyncio.get_running_loop()
         self._conn, _ = await loop.sock_accept(self._sock)
+        logging.info(f"Connection accepted")
 
     async def recv(self) -> str:
         """ Receive data from an existing connection """
         if self._conn is None:
+            logging.error("Socket unconnected")
             raise RuntimeError("Socket unconnected")
 
         loop = asyncio.get_running_loop()
         length, *_ = struct.unpack('I', await loop.sock_recv(self._conn, 4))
-        return (await loop.sock_recv(self._conn, length)).decode('utf-8')
+        ret = (await loop.sock_recv(self._conn, length)).decode('utf-8')
+        logging.info(f"Received {len(ret)}-byte message")
+        logging.debug(ret)
+        return ret
 
     async def send(self, content: str | bytes):
         """ Send data through an existing connection """
         if self._conn is None:
+            logging.error("Socket unconnected")
             raise RuntimeError("Socket unconnected")
 
         loop = asyncio.get_running_loop()
-        print(content)
         if isinstance(content, str):
             content = content.encode('utf-8')
 
         length = len(content)
         data = struct.pack('I', length) + content
         await loop.sock_sendall(self._conn, data)
+        logging.debug(f"Sent {len(data)}-byte message")
+        logging.debug(content.decode('utf-8'))
 
 class SocketHandler():
     """ Synchronous socket """
